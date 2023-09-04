d30f266445ac77ac84a831d588a1a99b972764b8
GAUTAM KUMAR <gkvc9696@gmail.com>
2023-09-04 19:43:05 +0530

    change google-analytic-dependency

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import { BookMarks } from "./pages/bookmark/BookMark";
 import { MobileNav } from "./component/mobileNav/MobileNav";
 import { SinglePost } from "./pages/singlepost/SinglePost";
 
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 import { useEffect } from "react";
 
 const GOOGLE_ANALYTIC_KEY = "G-57N32WKWCK";
@@ -33,7 +33,11 @@ function App() {
     });
   }
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname + window.location.search,
+      title: "Custom Title",
+    });
   }, []);
   return (
     <div className="App">
